9468e74d833745d5c2fd037bdafe8f974ab3cec3
Jennifer <jennifer.gd42@gmail.com>
2016-11-11 03:46:22 -0800

    All views are now class-based.

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post_detail'),
     # url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/new/$', views.PostCreateView.as_view(), name='post_new'),
-    url(r'^post/(?P<pk>\d+)/edit$', views.post_edit, name='post_edit'),
+    # url(r'^post/(?P<pk>\d+)/edit$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/edit$', views.PostUpdateView.as_view(), name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .models import Post
 from .forms import PostForm
 
@@ -27,74 +29,25 @@ class PostDetailView(generic.DetailView):
     template_name = "blog/post_detail.html"
 
 
-# def post_new(request):
-#     """View post creation form and create new posts."""
-
-#     # FIXME: Break into "display" and "process" routes; this hurts. T.T
-#     if request.method == 'POST':
-#         form = PostForm(request.POST)
-#         if form.is_valid():
-#             # Create a new Post instance with the form data, set its author
-#             # and pub date, and commit it to the db.
-#             post = form.save(commit=False)
-#             post.author = request.user
-#             post.published_date = timezone.now()
-#             post.save()
-
-#             # Take us to the post_detail page!
-#             return redirect('blog:post_detail', pk=post.pk)
-
-#     else:
-#         form = PostForm()
-
-#     return render(request, 'blog/post_edit.html', {'form': form})
-
-
-def post_edit(request, pk):
-    """Edit an existing post."""
-
-    # Try to fetch a post
-    post = get_object_or_404(Post, pk=pk)
-
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            #post.published_date = timezone.now()
-            post.save()
-            return redirect('blog:post_detail', pk=post.pk)
-
-    else:
-        form = PostForm(instance=post)
-
-    return render(request, 'blog/post_edit.html', {'form': form})
-
-
+# FIXME: Do I need to create an account/login route...?
+@method_decorator(login_required, name='dispatch')
 class PostCreateView(generic.CreateView):
     """Create a post."""
 
     template_name = "blog/post_edit.html"
-    form_invalid_message = "Please fill out all required fields."
-    form_valid_message = "Hooray!"
     form_class = PostForm
-    # success_url = "blog/post_detail"
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         form.instance.published_date = timezone.now()
         return super(PostCreateView, self).form_valid(form)
-#
-# class PostUpdateView(generic.UpdateView):
-#     """Class for post update form."""
-#
-#     template_name = "blog/post_edit.html"
-#     form_class = PostForm
-#     success_url = "blog/post_detail"
-#     form_invalid_message = "Please complete all required fields."
-#     form_valid_message = "Hooray! Your post was edited."
-#
-#     def get_queryset(self, queryset=None):
-#         """Get the existing post record."""
-#         return get_object_or_404(Post, pk=self.pk)
+
+class PostUpdateView(generic.UpdateView):
+    """Class for post update form."""
+
+    template_name = "blog/post_edit.html"
+    form_class = PostForm
+
+    def get_object(self, queryset=None):
+        """Get the existing post record."""
+        return get_object_or_404(Post, pk=self.kwargs["pk"])

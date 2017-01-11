c06fdb4367f08e73e18866758a02dc81a1fb0493
Jennifer <jennifer.gd42@gmail.com>
2017-01-10 20:12:08 -0800

    changed sec settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,22 +18,32 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = "/static/"
-MEDIA_URL = "/media/"
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ovyjimjx^n&q2zi@)__lepg29djk2v*7_4fg72l@t8nv$l_nc3'
+SECRET_KEY = os.environ['BLOG_SECRET_KEY']
+
+
+# Security settings suggested by check --deploy
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_HTTPONLY = True
+X_FRAME_OPTIONS = 'DENY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.jennifergd.com']
 
 
 # Application definition

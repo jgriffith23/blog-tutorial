90fcf3eaea5b8f7f400bd8c822219e3802f0b8cc
Jennifer <jennifer.gd42@gmail.com>
2017-02-03 18:13:52 -0800

    Change static url

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'blog/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),

f7aa2f0d2b913b264a91e6cb5ac155cc31ff79a5
Jennifer <jennifer.gd42@gmail.com>
2017-05-05 09:56:49 -0700

    Use better dict syntax

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ MEDIA_URL = '/media/'
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['BLOG_SECRET_KEY']
+SECRET_KEY = os.environ.get('BLOG_SECRET_KEY')
 
 
 # Security settings suggested by check --deploy

7542c16f76c3bcab4bc6a10a1b91cbb051d8dc9b
Jennifer <jennifer.gd42@gmail.com>
2017-05-05 19:17:49 -0700

    Fix hosts var

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', ['.jennifergd.com','172.31.30.183'])
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', [])
 DEBUG = os.environ.get('DEBUG', True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)

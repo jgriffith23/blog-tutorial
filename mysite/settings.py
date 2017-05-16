662fe25f2b798eb8cccf91708b8dba5169968947
jennifer.ec2 <jennifer.gd42@gmail.com>
2017-05-16 20:27:38 +0000

    Switch environment variable fetch to assume prod

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', [])
-DEBUG = os.environ.get('DEBUG', True)
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', ['.jennifergd.com', 172.31.30.183])
+DEBUG = os.environ.get('DEBUG', False)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
